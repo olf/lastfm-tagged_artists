426bcee78fe9a7d3894b4840dcd30b6541afb87a
Olaf Peters <olf@magicolf.de>
2016-02-22 20:50:29 +0100

    add input for user name

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-Get a list of artists tagged with a personal tag on last.fm
+## What's this? ##
 
-I'm tagging the list of bands that I've seen in concert with the 'seen live' tag on last.fm. As the current last.fm beta doesn't have an easy/obvious way to access tags (or get the list of artists tagged with 'seen live', for that matter), I've hacked this little script together.
+Small web form to get a list of artists tagged with a personal tag on last.fm using the API.
+
+## Usage ##
+
+Rename js/config.js.sample to js/config.js, insert your last.fm API credentials and default values for username and tag.
--- js/main.js
+++ js/main.js
@@ -1,34 +1,43 @@
-/* global lfmInstance, lfmUser */
+/* global lfmInstance, lfmDefaultUser, lfmDefaultTag */
+/* exported renderList, renderUserTags */
 /* eslint-env jquery */
 
-function renderList(theTag) {
+function showTaggedArtists() {
     var lastfm = lfmInstance();
+    var theUser = document.getElementById('user').value;
+    var theTag = document.getElementById('tag').value;
 
     lastfm.user.getPersonalTags(
         {
-            user: lfmUser(),
+            user: theUser,
             tag: theTag,
             taggingtype: 'artist',
-            limit: 500
+            limit: 1000
         },
-        {success: function(data) {
-            var list = data.taggings.artists.artist
-                .map(function (v) {return v.name;})
-                .sort(function (a, b) {
-                    return a.toLowerCase().localeCompare(b.toLowerCase());
-                })
-                .reduce(function (p, v) {
-                    return p + '<li>' + v + '</li>';
-                }, '');
+        {
+            success: function(data) {
+                var list = data.taggings.artists.artist
+                    .map(function (v) {return v.name;})
+                    .sort(function (a, b) {
+                        return a.toLowerCase().localeCompare(b.toLowerCase());
+                    })
+                    .reduce(function (p, v) {
+                        return p + '<li>' + v + '</li>';
+                    }, '');
 
-            document.getElementById('list').innerHTML = '<ol>' + list + '</ol>';
-        }},
-        {error: function(code, message) {
-            document.getElementById('list').innerHTML = 'Error: ' + message + ' (' + code +')';
-        }}
+                document.getElementById('list').innerHTML = '<ol>' + list + '</ol>';
+            }
+        },
+        {
+            error: function(code, message) {
+                document.getElementById('list').innerHTML = 'Error: ' + message + ' (' + code +')';
+            }
+        }
     );
 }
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     renderList('seen live');
-// });
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('user').value = lfmDefaultUser;
+    document.getElementById('tag').value  = lfmDefaultTag;
+    showTaggedArtists();
+});
